be8ef2fc66d3d3ca32fd2dc00008cfb844024d28
JulienLamalle <Julienlamalle63100@gmail.com>
2021-10-30 13:40:14 +0200

    added csv data reader

---
+++ data_reader/csv_data_reader.py
@@ -0,0 +1,16 @@
+import csv
+
+def csv_data_reader(file):
+    data_read = []
+    with open(file, newline="") as csvfile:
+        data = csv.DictReader(csvfile)
+        for share in data:
+            if float(share["price"]) > 0 and float(share["profit"]) > 0:
+                data_read.append(
+                    (
+                        share["name"],
+                        share["price"],
+                        share["profit"],
+                    )
+                )
+    return data_read
